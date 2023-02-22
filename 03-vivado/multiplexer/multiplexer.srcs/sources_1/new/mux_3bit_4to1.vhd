63fcd15b4a1f1c37d042f2ef9f4d9dc3f803917b
Mucha Vojtěch (240956) <240956@vutbr.cz>
2023-02-22 12:49:44 +0100

    cv_3

---
+++ 03-vivado/multiplexer/multiplexer.srcs/sources_1/new/mux_3bit_4to1.vhd
@@ -0,0 +1,55 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/22/2023 12:11:55 PM
+-- Design Name: 
+-- Module Name: mux_3bit_4to1 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity mux_3bit_4to1 is
+  Port ( 
+        sel_i         : in  std_logic_vector(2-1 downto 0);
+        a_i           : in  std_logic_vector(3-1 downto 0);
+        b_i           : in  std_logic_vector(3-1 downto 0);
+        c_i           : in  std_logic_vector(3-1 downto 0);
+        d_i           : in  std_logic_vector(3-1 downto 0);
+        f_o           : out std_logic
+  
+    );
+end entity mux_3bit_4to1;
+
+architecture Behavioral of mux_3bit_4to1 is
+
+begin
+with sell_i select
+    f_o <= a_i when "00",  -- If addr_i = "000" then y_o = a_i
+           b_i when "01",
+           c_i when "10",
+           d_i when others; -- All other combinations
+
+end Behavioral;
